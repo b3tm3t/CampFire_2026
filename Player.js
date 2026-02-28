e72d8462b94b35ab6c4c8a0414cf9b656680ce13
Austin Liu <eo123everyone@gmail.com>
2026-02-28 18:46:51 -0500

    adding angle calculations

--- Player.js
+++ Player.js
@@ -1,6 +1,6 @@
 
 
-class Player {
+export class Player {
     // Materials
     pos_x = 0;
     pos_y = 0;
@@ -8,12 +8,13 @@ class Player {
     speed = 0;
     width = 0;
 
-    angleVectorX = 0;
-    angleVectorY = 0;
+    currentAngle = 0;
 
     length = 1;
     health = 0;
     breath = 300;
+
+    angleTurning = 1;
 
     secondsSurvived = 0;
     
@@ -33,14 +34,34 @@ class Player {
 
     }
 
-    readInput(input) {
-        if (input.ArrowUp) this.pos_y -= this.speed;
-        if (input.ArrowDown) this.pos_y += this.speed;
-        if (input.ArrowLeft) this.pos_x -= this.speed;
-        if (input.ArrowRight) this.pos_x += this.speed;
+    calculateAngle(input) { 
+        
+        leftRight = input.ArrowLeft - input.ArrowRight;
+        upDown = input.ArrowUp - input.ArrowDown;
 
+        desiredAngle = Math.atan(upDown/leftRight);
+
+        angleDiff = (desiredAngle - this.currentAngle) % (Math.PI * 2); 
+
+        if (angleDiff > Math.PI) {
+
+        }
+
+
+
+
+        
 
     }
+
+    
+
+    draw(ctx) {
+        ctx.fillStyle = "#2ecc71"; // Green like your button
+        ctx.fillRect(this.pos_x, this.pos_y, this.width, this.width);
+    }
+
+
 
 
 
