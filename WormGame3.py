ad7a636a4ffc911bea97739c82c1f26ef3516a1c
AULA8693 <bsull6@ocdsb.ca>
2026-02-28 15:10:51 -0500

    change name + main window parameters

---
+++ WormGame3.py
@@ -0,0 +1,7 @@
+import pygame
+import os
+pygame.font.init()
+
+WIDTH, HEIGHT = 900, 500
+WINDOW = pygame.display.set_mode((WIDTH, HEIGHT))
+pygame.display.set_caption("Worm Game 3")
--- WyrmGame3.py
+++
@@ -1,5 +0,0 @@
-import pygame
-import os
-pygame.font.init()
-
-WIDTH, HEIGHT = 900, 500
